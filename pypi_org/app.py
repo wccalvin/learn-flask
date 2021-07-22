384a28a534268152292cc97f5a96cccbf78b9adf
wccalvin <clayton.calvin@gmail.com>
2021-07-22 09:38:50 -0400

    basic template rendering

--- pypi_org/app.py
+++ pypi_org/app.py
@@ -3,10 +3,19 @@ import flask
 app = flask.Flask(__name__)
 
 
+def get_latest_packages():
+    return [
+        {"name": "flask", "version": "1.2.3"},
+        {"name": "sqlalchemy", "version": "2.1.0"},
+        {"name": "passlib", "version": "0.3.2"},
+    ]
+
+
 @app.route("/")
 def index():
-    return "Hello World"
+    test_packages = get_latest_packages()
+    return flask.render_template("index.html", packages=test_packages)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
